e35e12c07b8973f9d4b0e43dcd9edfd39eaf02ff
Iryna Sokolovska <sokirina777@gmail.com>
2023-09-11 22:48:26 +0300

    fifth

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,5 +1,4 @@
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
-import React, { Component } from 'react';
 
 export const ImageGallery = ({ images }) => {
   return (
@@ -12,45 +11,3 @@ export const ImageGallery = ({ images }) => {
 };
 
 export default ImageGallery;
-
-// import { Gallery } from './ImageGallery.styled';
-// import { ImageGalleryItem } from './ImageGalleryItem';
-
-// export const ImageGallery = ({ hits }) => {
-//   return (
-//     <div>
-//       <Gallery className="gallery">
-//         {hits.map(({ id, largeImageURL, webformatURL, tags }) => (
-//           <ImageGalleryItem
-//             key={id}
-//             id={id}
-//             webformatURL={webformatURL}
-//             largeImageURL={largeImageURL}
-//             tags={tags}
-//           />
-//         ))}
-//       </Gallery>
-//     </div>
-//   );
-// };
-
-// import { Gallery } from './ImageGallery.styled';
-// import { ImageGalleryItem } from './ImageGalleryItem';
-
-// export const ImageGallery = ({ hits }) => {
-//   return (
-//     <div>
-//       <Gallery className="gallery">
-//         {hits.map(({ id, largeImageURL, webformatURL, tags }) => (
-//           <ImageGalleryItem
-//             key={id}
-//             id={id}
-//             webformatURL={webformatURL}
-//             largeImageURL={largeImageURL}
-//             tags={tags}
-//           />
-//         ))}
-//       </Gallery>
-//     </div>
-//   );
-// };
