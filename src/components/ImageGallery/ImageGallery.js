6a24579c9acfabebed1f074143a5760e9680d224
Iryna Sokolovska <sokirina777@gmail.com>
2023-09-12 06:11:13 +0300

    seventh

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,12 +1,28 @@
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
+import { GalleryUl } from './ImageGallery.styled';
 
-export const ImageGallery = ({ images, id }) => {
+export const ImageGallery = ({
+  images,
+  id,
+  webformatURL,
+  largeImageURL,
+  tags,
+}) => {
   return (
-    <ul className="gallery">
-      {images.map(image => (
-        <ImageGalleryItem key={image.id} id={id} image={image} />
-      ))}
-    </ul>
+    <div>
+      <GalleryUl className="gallery">
+        {images.map(image => (
+          <ImageGalleryItem
+            key={image.id}
+            id={id}
+            image={image}
+            webformatURL={webformatURL}
+            largeImageURL={largeImageURL}
+            tags={tags}
+          />
+        ))}
+      </GalleryUl>
+    </div>
   );
 };
 
